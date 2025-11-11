b33984bbc4d7d12e905ccf6f87c499f4764be393
JulRiv07 <Julian.rivera@utp.edu.co>
2025-11-11 17:06:21 -0500

    Actualizacion de la pagina, terminado el ¿como se produce?, ¿para que sirve?, tipos, como se produce, ventajas, footer y demas, tambien se actualizó la parte de tipos de forma que se hizo responsive y se le aplico java script para hacer un slider que permite ir viendo los diferentes tipos, quedan faltando las graficas e informacion de datasets, actualizado tambien el responsive para todas las sesiones haciendo falta llegar a un acuerdo de como hacer el inicio de la pagina, se modifico el tamaño de letra para el celular... Terminadas las graficas!

---
+++ Python/grafica2.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import plotly.graph_objects as go
+
+try:
+    df = pd.read_csv('13 installed-solar-PV-capacity.csv')
+except FileNotFoundError:
+    print("Error: No se encontró el archivo '13 installed-solar-PV-capacity.csv'")
+    exit()
+
+df['Solar Capacity'] = df['Solar Capacity'].astype(str).str.replace('.', '', regex=False)
+df['Solar Capacity'] = pd.to_numeric(df['Solar Capacity'])
+df_sorted = df.sort_values(by='Solar Capacity', ascending=False)
+
+
+
+fig = go.Figure(go.Pie(
+    labels=df_sorted['Entity'],       
+    values=df_sorted['Solar Capacity'], 
+    textinfo='percent+label'            
+))
+
+
+fig.update_layout(
+    title='Capacidad Solar Instalada por Región (2021)',
+    template='plotly_white'
+)
+
+
+fig.write_html('grafica_pastel_solar.html') 
+print("¡Gráfica interactiva 'grafica_pastel_solar.html' guardada con éxito!")
